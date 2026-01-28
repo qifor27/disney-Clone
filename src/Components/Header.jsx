c4883972c7c3f82e882bc51381f0607449df6d23
qifor27 <rofiqislamy88@gmail.com>
2026-01-28 14:50:15 +0700

    add responsive dropdown menu for mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import logo from '../assets/images/logo.png'
 import {
     HiHome,
@@ -11,6 +12,7 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from './HeaderItem'
 
 function Header() {
+   const [toggle,setToggle] = useState(false);
     const menu = [
         {
             name: 'HOME',
@@ -39,14 +41,34 @@ function Header() {
     ]
     return (
         <div className='flex items-center justify-between p-5'>
-            <div className='flex gap-8 items-center'>           
-            <img src={logo} alt="logo"
-                className='w-[80px] md:w-[115px] object-cover' />
-            {menu.map((item) => (
-                <HeaderItem name={item.name} Icon={item.icon} />
-            ))}
-             </div>
-             <img src={logo} alt="avatar" className = 'w-[40px] rounded-full' />
+            <div className='flex gap-8 items-center'>
+                <img src={logo} alt="logo"
+                    className='w-[80px] md:w-[115px] object-cover' />
+
+                <div className='hidden md:flex gap-8'>
+                    {menu.map((item) => (
+                        <HeaderItem name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
+
+                <div className='flex md:hidden gap-5'>
+                    {menu.map((item, index) => index < 3 && (
+                        <HeaderItem name={''} Icon={item.icon} />
+                    ))}
+                    <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+                        <HeaderItem name={''} Icon={HiDotsVertical} />
+                    {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3
+                        px-5 py-4 '>
+                            {menu.map((item, index) => index > 2 && (
+                                <HeaderItem name={item.name} Icon={item.icon}  />
+                            ))}
+                        </div> :null}
+                    </div> 
+
+                </div>
+
+            </div>
+            <img src={logo} alt="avatar" className='w-[40px] rounded-full' />
         </div>
     )
 }
